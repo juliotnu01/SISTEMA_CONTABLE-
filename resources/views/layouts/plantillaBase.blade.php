cbbab9f4385d9ea8cbea6b52d6bfe127323330c2
Diego Vallejo <dyegovallejob@gmail.com>
2019-08-26 12:33:27 -0500

    puc bug

--- resources/views/layouts/plantillaBase.blade.php
+++ resources/views/layouts/plantillaBase.blade.php
@@ -81,7 +81,7 @@
         <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTerceros" aria-expanded="true" aria-controls="collapseTerceros">
                 <i class="fas fa-fw fa-wrench"></i>
-                <span>Terceros y Dependencias</span>
+                <span>Terceros y Dependenciass</span>
             </a>
             <div  id="collapseTerceros" class="collapse" aria-labelledby="collapseTerceros" data-parent="#accordionSidebar">
                 <div  class="bg-white py-2 collapse-inner rounded submenu">
